9b83437107b2266aed28a5f723bc20b1c3195a53
Александр <grebnev.ass@mail.ru>
2022-01-31 23:13:24 +0300

    Lesson_1

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,15 @@
+public class HomeWorkApp {
+
+
+    public static void main(String[] args) {
+
+        Test printThreeWords = new Test();
+        Test.printThreeWords();
+        Test.сheckSumSign();
+        Test.printColor();
+        Test.compareNumbers();
+
+    }
+
+
+}
